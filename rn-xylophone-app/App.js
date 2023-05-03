cf1c5f02b55cc173315cf9e1916e82d9f67704ac
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2023-05-03 16:40:48 -0400

    app updated

--- rn-xylophone-app/App.js
+++ rn-xylophone-app/App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Xylophone from './Xylophone';
+import Record from './Record';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+// seven different sound files, one for each "button"/xylophone bar
+const xyloSounds = {
+	one: require('./assets/note1.wav'),
+	two: require('./assets/note2.wav'),
+	three: require('./assets/note3.wav'),
+	four: require('./assets/note4.wav'),
+	five: require('./assets/note5.wav'),
+	six: require('./assets/note6.wav'),
+	seven: require('./assets/note7.wav')
+}
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Xylophone" component={Xylophone} />
+				<Stack.Screen name="Record" component={Record} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		borderRadius: 25,
+		marginTop: 50
+	},
+	buttonContainer: {
+		height: 40,
+		margin: 5
+	},
+	button: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	buttonText: {
+		color: '#fff',
+		fontSize: 18
+	}
 });
+
+export default App;
